22052cde2215bca1bb06d328222772a0c6b65f15
Adithya <adithyam866@gmail.com>
2022-06-08 10:35:59 +0530

    update simple goto

--- 7_simple_goto.py
+++ 7_simple_goto.py
@@ -10,45 +10,33 @@ vehicle = connect('127.0.0.1:14551',baud=921600,wait_ready=True)
 def arm_takeoff(height):
     #check if drone is ready
     while not vehicle.is_armable:
-        print("waiting for drone")
+        print("Waiting for drone")
         time.sleep(1)
 
     #change mode and arm
-    print("arming")
+    print("Arming")
     vehicle.mode=VehicleMode('GUIDED')
     vehicle.armed=True
     
     #check if drone is armed
     while not vehicle.armed:
-        print("Waiting for arm")
+        print("Waiting for arm\n")
         time.sleep(1)
 
     #takeoff
-    print("takeoff")
+    print("Taking off")
     vehicle.simple_takeoff(height)
 
     #report values back every 1s and finally break out
     while True:
         print('Reached ',vehicle.location.global_relative_frame.alt)
         if(vehicle.location.global_relative_frame.alt>=height*0.95):
-            print("Reached Target Altitude")
+            print("Reached Target Altitude\n")
             break
         time.sleep(1)
 
 def get_location_metres(original_location, dNorth, dEast):
-    """
-    Returns a LocationGlobal object containing the latitude/longitude `dNorth` and `dEast` metres from the 
-    specified `original_location`. The returned LocationGlobal has the same `alt` value
-    as `original_location`.
 
-    The function is useful when you want to move the vehicle around specifying locations relative to 
-    the current vehicle position.
-
-    The algorithm is relatively accurate over small distances (10m within 1km) except close to the poles.
-
-    For more information see:
-    http://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
-    """
     earth_radius = 6378137.0 #Radius of "spherical" earth
     #Coordinate offsets in radians
     dLat = dNorth/earth_radius
@@ -67,56 +55,38 @@ def get_location_metres(original_location, dNorth, dEast):
     return targetlocation;
 
 def get_distance_metres(aLocation1, aLocation2):
-    """
-    Returns the ground distance in metres between two LocationGlobal objects.
 
-    This method is an approximation, and will not be accurate over large distances and close to the 
-    earth's poles. It comes from the ArduPilot test code: 
-    https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
-    """
     dlat = aLocation2.lat - aLocation1.lat
     dlong = aLocation2.lon - aLocation1.lon
     return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
 
 def goto(dNorth, dEast, gotoFunction=vehicle.simple_goto):
-    """
-    Moves the vehicle to a position dNorth metres North and dEast metres East of the current position.
 
-    The method takes a function pointer argument with a single `dronekit.lib.LocationGlobal` parameter for 
-    the target position. This allows it to be called with different position-setting commands. 
-    By default it uses the standard method: dronekit.lib.Vehicle.simple_goto().
-
-    The method reports the distance to target every two seconds.
-    """
-    
     currentLocation = vehicle.location.global_relative_frame
     targetLocation = get_location_metres(currentLocation, dNorth, dEast)
     targetDistance = get_distance_metres(currentLocation, targetLocation)
     gotoFunction(targetLocation)
     
-    #print "DEBUG: targetLocation: %s" % targetLocation
-    #print "DEBUG: targetLocation: %s" % targetDistance
-
     while vehicle.mode.name=="GUIDED": #Stop action if we are no longer in guided mode.
-        #print "DEBUG: mode: %s" % vehicle.mode.name
         remainingDistance=get_distance_metres(vehicle.location.global_relative_frame, targetLocation)
         print("Distance to target: ", remainingDistance)
         if remainingDistance<=targetDistance*0.01: #Just below target, in case of undershoot.
-            print("Reached target")
+            print("Reached target\n")
             break;
         time.sleep(2)
 
 #takeoff
 arm_takeoff(10)
 
-#move 10m North and 20m East
-goto(10,20)
-
+#move 100m North and 200m East
+print('Moving to target location')
+goto(100,200)
 
 #landing
-print('Land')
+print('Landing')
 vehicle.mode=VehicleMode('RTL')
 time.sleep(20)
 
 #close vehicle
+print('Done')
 vehicle.close()
